5b4507f5651a9dc3ab312ac8fdd59b72167196df
Rossi21 <rossi21@gmail.com>
2023-04-02 09:51:39 -0700

    first commit

---
+++ src/main/java/com/rossi21/dojos/DojosApplication.java
@@ -0,0 +1,13 @@
+package com.rossi21.dojos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DojosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DojosApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/rossi21/dojos/models/Ninja.java
@@ -0,0 +1,109 @@
+package com.rossi21.dojos.models;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+import javax.persistence.Table;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+@Entity 
+@Table(name="ninjas")
+public class Ninja {
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotNull
+    @Size(min = 2, max = 200, message="First name must be at least 2 characters.")
+    private String first_name;
+    @NotNull
+    @Size(min = 2, max = 200, message="Last name must be at least 2 characters.")
+    private String last_name;
+    @NotNull
+    @Min(value = 18, message = "Age should not be less than 18")
+    @Max(value = 120, message = "Age should not be greater than 120")
+    private Integer age;
+    // This will not allow the createdAt column to be updated after creation
+    @Column(updatable=false)
+    @DateTimeFormat(pattern="yyyy-MM-dd")
+    private Date createdAt;
+    @DateTimeFormat(pattern="yyyy-MM-dd")
+    private Date updatedAt;
+    //Allows our one to many connection with the Dojo class
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="dojo_id")
+    private Dojo dojo;
+    
+    public Ninja() {
+    }
+    public Ninja(String first_name, String last_name, Integer age) {
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.age = age;    
+    }
+    
+    @PrePersist
+    protected void onCreate(){
+        this.createdAt = new Date();
+    }
+    @PreUpdate
+    protected void onUpdate(){
+        this.updatedAt = new Date();
+    }
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getFirst_name() {
+		return first_name;
+	}
+	public void setFirst_name(String first_name) {
+		this.first_name = first_name;
+	}
+	public String getLast_name() {
+		return last_name;
+	}
+	public void setLast_name(String last_name) {
+		this.last_name = last_name;
+	}
+	public Integer getAge() {
+		return age;
+	}
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+	public void setCreatedAt(Date createdAt) {
+		this.createdAt = createdAt;
+	}
+	public Date getUpdatedAt() {
+		return updatedAt;
+	}
+	public void setUpdatedAt(Date updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+	public Dojo getDojo() {
+		return dojo;
+	}
+	public void setDojo(Dojo dojo) {
+		this.dojo = dojo;
+	}
+    
+
+}
